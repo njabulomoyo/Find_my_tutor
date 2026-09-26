fd100e37496cd25051b03e190105a3eefc9d13df
Njabulo <njabulomoyoe@gmail.com>
2026-09-26 11:30:43 -0500

    Added tutors' email + availability times

--- backend/src/data/tutors.js
+++ backend/src/data/tutors.js
@@ -3,6 +3,7 @@ const tutors = [
     id: 1,
     name: 'Adriel Dube',
     image: null,
+    email: 'adube@gsumail.gram.edu',
     major: 'Computer Science & Cloud Computing',
     classification: 'Senior',
     subjects: ['Pre-Calculus', 'Calculus I', 'Probability and Statistics', 'Data Structures and Algorithms', 'Computer Science I', 'Computer Science II'],
@@ -12,46 +13,56 @@ const tutors = [
     id: 2,
     name: 'Enoch Owoade',
     image: null,
+    email: 'eowoade@gsumail.gram.edu',
     major: 'Computer Engineering',
     classification: 'Senior',
     subjects: ['Engineering', 'Calculus II','Calculus III', 'Probability and Statistics','Data Structures and Algorithms' ],
-    availability: ['Tue 9:00 AM - 12:00 PM', 'Thu 1:00 PM - 4:00 PM']
+    availability: ['Mon 12:00 PM - 5:00 PM','Tue 11:00 AM - 5:00 PM', 'Wed 3:00 PM - 5:00 PM',
+      'Thu 12:00 AM - 5:00 PM', 'Fri 11:00 AM - 1:00 PM'
+    ]
+    
   },
   {
     id: 3,
     name: 'Maitaishe Mangudhla',
     image: null,
+    email: 'mmangund@gsumail.gram.edu',
     major: 'Biology',
     classification: 'Graduate',
     subjects: ['General Chemistry I','General Chemistry II', 'Biology I','Anatomy and Physiology I', 'pre-Calculus', 'Calculus I'],
-    availability: ['Mon 10:00 AM - 1:00 PM', 'Fri 2:00 PM - 5:00 PM']
+    availability: [ 'Mon 10:00 AM - 12:00 PM & 3:30PM – 5:00PM','Tue 11:00 AM - 2:00 PM', 'Wed 11:00 AM - 12:00 PM and 3:30PM - 5:00PM',
+      'Thu 11:00 AM - 5:00 PM', 'Fri 9:00 AM - 1:00 PM']
   },
   {
     id: 4,
     name: 'Njabulo Moyo',
     image: null,
+    email: 'nmoyo@gsumail.gram.edu',
     major: 'Computer Science',
     classification: 'Senior',
-    subjects: ['Computer Science I', 'Computer Science II', 'Data Structures and Algorithms', 'Probability and Statistics', 'Calculus '],
-    availability: ['Tue 2:00 PM - 5:00 PM', 'Thu 9:00 AM - 12:00 PM']
+    subjects: ['Computer Science I', 'Computer Science II', 'Data Structures and Algorithms', 'Probability and Statistics', 'Pre-Calculus'],
+    availability: ['Mon 10:00 AM - 3:00 PM & 4:30PM – 5:00PM','Tue 11:00 AM - 1:00 PM & 3:00PM – 5:00PM', 'Wed 11:00 AM - 3:00 PM & 3:00PM - 5:00PM',
+      'Thu 11:00 AM - 1:00 PM & 3:00PM – 5:00PM', 'Fri 9:00 AM - 10:00 AM & 11:00 AM - 1:00 PM']
   },
   {
     id: 5,
     name: 'Iyanuoluwa Fagbamila',
     image: null,
+    email: 'ifagbami@gsumail.gram.edu',
     major: 'Nursing',
     classification: 'Senior',
     subjects: ['Anatomy and Physiology I', 'Pathophysiology', 'Microbiology', 'College Algebra'],
-    availability: ['Mon 9:00 AM - 12:00 PM', 'Wed 2:00 PM - 5:00 PM']
+    availability: ['Tue 11:00 AM - 5:00 PM', 'Wed 10:00 AM - 4:00 PM', 'Thu 10:00 AM - 5:00 PM',]
   },
   {
     id: 6,
     name: 'Stecy Chirinda',
     image: null,
+    email: 'schirind@gsumail.gram.edu',
     major: 'Math and Physics',
-    classification: 'Graduate',
+    classification: 'Senior',
     subjects: ['Pre-Calculus I','Pre-Calculus II','Trigonometry','Calculus I','General Psychology'],
-    availability: ['Wed 9:00 AM - 12:00 PM', 'Fri 1:00 PM - 4:00 PM']
+    availability: ['Mon 11:30 AM - 4:30 PM','Tue 12:00 PM - 5:00 PM', 'Wed 11:30 AM - 4:30 PM', 'Thu 12:00 AM - 5:00 PM']
   }
 ];
 
